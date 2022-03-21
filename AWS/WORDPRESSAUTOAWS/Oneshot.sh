58b58d6d6d1c73832ffbc106e40ca835aae8f6df
jlornem <66388277+jlgito@users.noreply.github.com>
2022-03-21 20:36:57 +0100

    Update Oneshot.sh

--- AWS/WORDPRESSAUTOAWS/Oneshot.sh
+++ AWS/WORDPRESSAUTOAWS/Oneshot.sh
@@ -16,3 +16,16 @@ aws ec2 describe-route-tables --route-table-id rtb-c1c8faa6
 aws ec2 associate-route-table  --subnet-id subnet-b46032ec --route-table-id rtb-c1c8faa6
 aws ec2 modify-subnet-attribute --subnet-id subnet-b46032ec --map-public-ip-on-launch
 
+#https://docs.aws.amazon.com/cli/latest/userguide/cli-services-ec2-sg.html
+
+aws ec2 create-security-group --group-name my-sg --description "My security group" --vpc-id vpc-1a2b3c4d
+aws ec2 authorize-security-group-ingress --group-id sg-903004f8 --protocol tcp --port 3389 --cidr x.x.x.x
+
+#aws ec2 authorize-security-group-ingress --group-name my-sg --protocol tcp --port 3389 --cidr x.x.x.x
+
+
+#https://docs.aws.amazon.com/cli/latest/userguide/cli-services-ec2-instances.html
+#https://docs.aws.amazon.com/cli/latest/reference/opsworks/create-instance.html
+
+aws opsworks --region us-east-1 create-instance --stack-id 935450cc-61e0-4b03-a3e0-160ac817d2bb --layer-ids 5c8c272a-f2d5-42e3-8245-5bf3927cb65b --hostname wordpress --instance-type m1.large --os "Amazon Linux"
+aws opsworks --region us-east-1 create-instance --stack-id 935450cc-61e0-4b03-a3e0-160ac817d2bb --layer-ids 5c8c272a-f2d5-42e3-8245-5bf3927cb65b --hostname mariadb --instance-type m1.large --os "Amazon Linux"
