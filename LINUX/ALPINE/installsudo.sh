ecb63b9f4437e51523fa8ab26a085260823f53c2
jlornem <66388277+jlgito@users.noreply.github.com>
2022-03-25 02:49:46 +0100

    Update installsudo.sh

--- LINUX/ALPINE/installsudo.sh
+++ LINUX/ALPINE/installsudo.sh
@@ -1,1 +1,7 @@
 #!/bin/bash
+apk add sudo 
+echo '%wheel ALL=(ALL) ALL' > /etc/sudoers.d/wheel
+adduser joe wheel
+sudo -i
+sudo command with arguments
+#source : https://docs.alpinelinux.org/user-handbook/0.1a/Working/post-install.html
