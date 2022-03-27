3ec6c0b4d9ae1a0ea15c2486bddfe7127bd6d0f2
jlornem <66388277+jlgito@users.noreply.github.com>
2022-03-27 17:07:37 +0200

    Create scriplxc.sh

---
+++ LINUX/UBUNTU/MULTIPASS/scriplxc.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+multipass shell 
+multipass delete -p primary
+multipass list 
+multipass launch -c 2 -d 100G -m 8G -n lxdh1
+mulitpass shell lxdh1
+sudo apt update -y 
+sudo apt install lxcd
+
+lxd init
+Would you like to use LXD clustering? (yes/no) [default=no]:
+Do you want to configure a new storage pool? (yes/no) [default=yes]: yes
+Name of the new storage pool [default=default]:
+Name of the storage backend to use (dir, lvm, zfs, ceph, btrfs) [default=zfs]:
+Create a new ZFS pool? (yes/no) [default=yes]:
+Would you like to use an existing empty block device (e.g. a disk or partition)? (yes/no) [default=no]:
+Size in GB of the new loop device (1GB minimum) [default=19GB]:
+Would you like to connect to a MAAS server? (yes/no) [default=no]:
+Would you like to create a new local network bridge? (yes/no) [default=yes]:
+What should the new bridge be called? [default=lxdbr0]:
+What IPv4 address should be used? (CIDR subnet notation, “auto” or “none”) [default=auto]: 10.10.10.1/24
+Would you like LXD to NAT IPv4 traffic on your bridge? [default=yes]:
+What IPv6 address should be used? (CIDR subnet notation, “auto” or “none”) [default=auto]: none
+Would you like the LXD server to be available over the network? (yes/no) [default=no]:
+Would you like stale cached images to be updated automatically? (yes/no) [default=yes]
+Would you like a YAML "lxd init" preseed to be printed? (yes/no) [default=no]:
+
+lxc list 
+lxc image list images:ubuntu/focal/cloud
+#lxc delete -f <nom du conteneur>
+lxc image list images:amazon | grep CONT | grep x86
+lxc image copy images:amazonlinux local: --alias amzl
+lxc image list 
+lxc init amzl webserver
+lxc init amzl bdd
+lxc start webserver 
+lxc start amzl lauch bdd 
+lxc shell webserver 
+lxc shell amzl lauch bdd 
