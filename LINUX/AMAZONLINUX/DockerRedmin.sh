3eb8e9673a1c9e871e6e573ba25a88a7c99005ff
jlornem <66388277+jlgito@users.noreply.github.com>
2022-03-28 19:47:59 +0200

    Create DockerRedmin.sh

---
+++ LINUX/AMAZONLINUX/DockerRedmin.sh
@@ -0,0 +1,10 @@
+!#/bin/bash
+sudo yum update -y && yum update -y
+sudo yum install docker -y
+sudo systemctl start docker 
+sudo systemctl enable docker 
+sudo systemctl status docker 
+sudo docker container run -d --name redmine1 -p 80:3000 redmine
+ss -tlnp
+docker ps 
+#sudo docker contianer stop  redmine1
