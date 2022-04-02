1be565a13004c2013436281684a79fd141845a61
jlornem <66388277+jlgito@users.noreply.github.com>
2022-04-02 11:33:21 +0200

    Update bastionsmbwrdcontent.sh

--- LINUX/AMAZONLINUX/bastionsmbwrdcontent.sh
+++ LINUX/AMAZONLINUX/bastionsmbwrdcontent.sh
@@ -2,19 +2,16 @@
 #######################################################################################
 ########################Partie 1 : FOICHIER WORDPRESS UNIQUEMENT#######################
 #######################################################################################
+mkdir -p /mnt/efs/fs1/var/www/html
 sudo curl -O https://wordpress.org/latest.tar.gz
-sudo tar zxvf latest.tar.gz -C /var/www/html
-sudo cp /var/www/html/wordpress/wp-config-sample.php /var/www/html/wordpress/wp-config.php
-sudo sed -i 's/database_name_here/wordpress/' /var/www/html/wordpress/wp-config.php
-sudo sed -i 's/username_here/wp_user/' /var/www/html/wordpress/wp-config.php
-sudo sed -i 's/password_here/paSSw0rdSecured/' /var/www/html/wordpress/wp-config.php
-sudo sed -i 's/localhost/db1/' /var/www/html/wordpress/wp-config.php
-rm latest.tar.gz 
-sudo chown -R apache /var/www
-sudo chgrp -R apache /var/www
-sudo chmod 2775 /var/www && find /var/www -type d -exec sudo chmod 2775 {} \;
+sudo tar zxvf latest.tar.gz -C /mnt/efs/fs1/var/www/html
+sudo cp /var/www/html/wordpress/wp-config-sample.php /mnt/efs/fs1/var/www/html/wordpress/wp-config.php
+sudo sed -i 's/database_name_here/wordpress/' /mnt/efs/fs1/var/www/html/wordpress/wp-config.php
+sudo sed -i 's/username_here/main/' /mnt/efs/fs1/var/www/html/wordpress/wp-config.php
+sudo sed -i 's/password_here/lab-password/' /mnt/efs/fs1/var/www/html/wordpress/wp-config.php
+sudo sed -i 's/localhost/db1/' /mnt/efs/fs1/var/www/html/wordpress/wp-config.php 
+sudo chmod 2775 /mnt/efs/fs1/var/www && find /mnt/efs/fs1/var/www -type d -exec sudo chmod 2775 {} \;
 find /var/www -type f -exec sudo chmod 0664 {} \;
-sudo systemctl restart httpd
 #########################################################################################
 #################################PARTIE 2: deploiement smb################################
 ##########################################################################################
