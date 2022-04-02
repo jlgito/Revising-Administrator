4f8052824a0c1e197b99c790e1a39cf740ba961f
jlornem <66388277+jlgito@users.noreply.github.com>
2022-04-02 11:56:18 +0200

    Update bastionsmbwrdcontent.sh

--- LINUX/AMAZONLINUX/bastionsmbwrdcontent.sh
+++ LINUX/AMAZONLINUX/bastionsmbwrdcontent.sh
@@ -12,7 +12,7 @@ sudo sed -i 's/username_here/main/' /mnt/efs/fs1/var/www/html/wordpress/wp-confi
 sudo sed -i 's/password_here/lab-password/' /mnt/efs/fs1/var/www/html/wordpress/wp-config.php
 sudo sed -i 's/localhost/db1/' /mnt/efs/fs1/var/www/html/wordpress/wp-config.php 
 sudo chmod 2775 /mnt/efs/fs1/var/www && find /mnt/efs/fs1/var/www -type d -exec sudo chmod 2775 {} \;
-find /var/www -type f -exec sudo chmod 0664 {} \;
+find /mnt/efs/fs1/var/www/html/wordpress -type f -exec sudo chmod 0664 {} \;
 #########################################################################################
 #################################PARTIE 2: deploiement smb################################
 ##########################################################################################
