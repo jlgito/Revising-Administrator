6c64d7ad5573325389b300204931e3fd660e16b2
jlornem <66388277+jlgito@users.noreply.github.com>
2022-04-02 13:10:19 +0200

    Update apachetowordpresssmb.sh

--- LINUX/AMAZONLINUX/apachetowordpresssmb.sh
+++ LINUX/AMAZONLINUX/apachetowordpresssmb.sh
@@ -2,3 +2,27 @@
 #######################################################################################
 ########################Partie 1 : FOICHIER WORDPRESS UNIQUEMENT#######################
 #######################################################################################
+sudo yum update -y
+sudo yum install -y wget tar
+sudo amazon-linux-extras install -y lamp-mariadb10.2-php7.2 php7.2
+sudo yum install -y httpd
+sudo systemctl enable --now httpd
+echo "<?php phpinfo(); ?>" > /var/www/html/phpinfo.php
+yum install samba-client -y
+#######################################################################################
+########################Partie 2 : Fichir de configuration smbclient#######################
+#######################################################################################
+mkdir -p /mnt/linuxshare
+#######################################################################################
+########################Partie 2 : Fichir de configuration smbclient#######################
+#######################################################################################
+mkdir -p /etc/linux-credential
+cd /etc/linux-credential
+echo -e "username=user\npassword=password" >> credential
+sudo mount -t cifs -o credentials=/etc/linux-credential //WIN_SHARE_IP/<share_name> mkdir -p /mnt/linuxshare
+/etc/fstab
+#######################################################################################
+########################Partie 2 : Modification d'APACHE         #######################
+#######################################################################################
+sed -i 
+sed -i 
