1df3f544eeaa1caa2ce2b53f477f5af8de403187
jlornem <66388277+jlgito@users.noreply.github.com>
2022-04-07 03:29:40 +0200

    Create maria.py

---
+++ Python/Backend/maria.py
@@ -0,0 +1,21 @@
+#https://mariadb.com/fr/resources/blog/how-to-connect-python-programs-to-mariadb/
+# Module Imports
+import mariadb
+import sys
+
+# Connect to MariaDB Platform
+try:
+    conn = mariadb.connect(
+        user="db_user",
+        password="db_user_passwd",
+        host="192.0.2.1",
+        port=3306,
+        database="employees"
+
+    )
+except mariadb.Error as e:
+    print(f"Error connecting to MariaDB Platform: {e}")
+    sys.exit(1)
+
+# Get Cursor
+cur = conn.cursor()
